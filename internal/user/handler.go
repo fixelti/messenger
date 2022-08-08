64fa787e1031c6bca8e25ecbe8f965656a7075a9
fixel <fixelti@gmail.com>
2022-08-08 22:39:16 +0300

    Added create user func

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -1,5 +1,11 @@
 package user
 
+import (
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+	"net/http"
+)
+
 const (
 	userURL = "/users"
 )
@@ -7,4 +13,50 @@ const (
 type handler struct {
 	logger     *logging.Logger
 	repository Repository
+}
+
+type IDRequest struct {
+	UserID uint `uri:"user_id" binding:"required",min=1`
+}
+
+// Разобраться в этом блоке кода
+func NewHandler(logger *logging.Logger, repository Repository) handler.Handler {
+	return &handler{
+		logger:     logger,
+		repository: repository,
+	}
+}
+
+func (h *handler) Register(router *gin.RouterGroup) {
+	users := router.Group(userURL)
+	{
+		users.POST("", apperror.Middleware(h.Create))
+		//...//
+	}
+}
+
+func (h *handler) Create(c *gin.Context) error {
+	var userDTO CreateUserDTO
+
+	if err := c.ShouldBindJSON(&userDTO); err != nil {
+		return err
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(userDTO.Password), bcrypt.MinCost)
+	if err != nil {
+		return err
+	}
+
+	newUser, err := h.repository.Create(User{
+		Email:      userDTO.Email,
+		Login:      userDTO.Login,
+		Password:   string(hashedPassword),
+		SecretWord: userDTO.SecretWord,
+	})
+	if err != nil {
+		return apperror.NewAppError(nil, "new app error", "NEW-0000001")
+	}
+
+	c.JSON(http.StatusOK, newUser)
+	return nil
 }
